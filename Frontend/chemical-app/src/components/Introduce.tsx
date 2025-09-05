6c833561bca6b1347007af424d937fb8e47dceb6
Shayan-Mst <shnbackup007@gmail.com>
2025-09-06 01:12:43 +0330

    changed theme-color meta and made call button to call

--- Frontend/chemical-app/src/components/Introduce.tsx
+++ Frontend/chemical-app/src/components/Introduce.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom'
 import Button from './Button'
 
 const Introduce = () => {
@@ -22,10 +23,14 @@ const Introduce = () => {
                        جستجوی محصولات
                         
                    </Button>
-                   <Button variant='blueWithBorder' size=''>
+                   
+                   <Button variant='blueWithBorder' size='' onClick={() => (window.location.href = "tel:+989123454003")}>
+                    
                       <i className="ml-2" data-fa-i2svg=""><svg className="svg-inline--fa fa-phone" width="16" height="16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"></path></svg></i>
-                    درخواست بدهید
+                    تماس بگیرید
+                  
                    </Button>
+            
                 </div>
             </div>
             <div className="lg:block">
